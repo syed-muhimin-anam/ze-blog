ca5da577c4f5fa42e60a84f5b3fba07fc7c42f95
Syed Muhimin Anam <76765021+syed-muhimin-anam@users.noreply.github.com>
2025-06-15 20:16:50 +0600

    fix the bug

--- src/lib/actions/user.js
+++ src/lib/actions/user.js
@@ -1,46 +1,33 @@
 import User from "../mongodb/models/user.model";
-
 import { connect } from "../mongodb/mongoose";
 
-
-export const createOrUpdateUser = async(
-    id,
-    first_name,
-    last_name,
-    image_url,
-    email_addresses,
-    userName
-
+export const createOrUpdateUser = async (
+  id,
+  first_name,
+  last_name,
+  image_url,
+  email,
+  userName
 ) => {
-    try {
-        await connect();
-        const user = await User.findByIdAndUpdate(
-            {clerkId: id},
-            {
-                $set: {
-                    firstName: first_name,
-                    lastName: last_name,
-                    profilePicture:image_url,
-                    email: email_addresses[0].email_address,
-                    userName,
-                }
+  try {
+    await connect();
 
-            }, {new:true, upsert: true}
-        );
-        return user;
-    } catch (error) {
-        console.log('Error creating or updating user:', error);
-        
-        
-    }
+    const user = await User.findOneAndUpdate(
+      { clerkId: id }, // ✅ clerkId দিয়ে খোঁজা হচ্ছে
+      {
+        $set: {
+          firstName: first_name,
+          lastName: last_name,
+          profilePicture: image_url,
+          email: email,
+          userName,
+        },
+      },
+      { new: true, upsert: true } // ✅ না থাকলে তৈরি করবে, থাকলে আপডেট করবে
+    );
+
+    return user;
+  } catch (error) {
+    console.log('❌ Error creating or updating user:', error);
+  }
 };
-
-
-export const deleteUser = async (id) => {
-    try {
-        await connect();
-        await User.findOneAndDelete({clerkId: id})
-    } catch (error) {
-        console.log('Error creating or updating user:', error);
-    }
-}
